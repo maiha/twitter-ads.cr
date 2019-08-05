625d4ad383057d6782a1261e05a50bf0ad7de8d6
maiha <maiha@wota.jp>
2019-08-06 01:55:11 +0900

    pb: fix tracking_tag

---
+++ src/twitter-ads/proto/tracking_tag.pb.cr
@@ -0,0 +1,24 @@
+## Generated from proto/tracking_tag.proto
+require "protobuf"
+
+module TwitterAds
+  module Proto
+    
+    struct TrackingTag
+      include Protobuf::Message
+      
+      contract_of "proto2" do
+        optional :tracking_partner, :string, 1
+        optional :tracking_tag, :string, 2
+      end
+    end
+    
+    struct TrackingTagArray
+      include Protobuf::Message
+      
+      contract_of "proto2" do
+        repeated :array, TrackingTag, 1
+      end
+    end
+    end
+  end
