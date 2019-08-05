06415c6f6ac2abca833a3a8676e3a19aeb916deb
maiha <maiha@wota.jp>
2019-08-05 12:05:24 +0900

    add Proto::PromotedTweet

---
+++ src/twitter-ads/proto/promoted_tweet.pb.cr
@@ -0,0 +1,30 @@
+## Generated from proto/promoted_tweet.proto
+require "protobuf"
+
+module TwitterAds
+  module Proto
+    
+    struct PromotedTweet
+      include Protobuf::Message
+      
+      contract_of "proto2" do
+        optional :id, :string, 1
+        optional :line_item_id, :string, 2
+        optional :tweet_id, :string, 3
+        optional :approval_status, :string, 4
+        optional :entity_status, :string, 5
+        optional :created_at, :string, 6
+        optional :updated_at, :string, 7
+        optional :deleted, :bool, 8
+      end
+    end
+    
+    struct PromotedTweetArray
+      include Protobuf::Message
+      
+      contract_of "proto2" do
+        repeated :array, PromotedTweet, 1
+      end
+    end
+    end
+  end
