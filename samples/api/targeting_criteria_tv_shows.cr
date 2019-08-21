8303787781c41686f9ba69209a060406e19a9ecb
maiha <maiha@wota.jp>
2019-08-22 03:07:17 +0900

    samples: add detailed values for locale

--- samples/api/targeting_criteria_tv_shows.cr
+++ samples/api/targeting_criteria_tv_shows.cr
@@ -1,6 +1,7 @@
 require "../../src/twitter-ads"
 
 client = TwitterAds::Client.from_twurlrc
-client.targeting_criteria_tv_shows("en-US", q: "news", count: 3).each do |a|
+locale = "en-US" # "ja-JP"
+client.targeting_criteria_tv_shows(locale, q: "news", count: 3).each do |a|
   puts "TargetingCriteriaTvShow(%s) # %s" % [a.id, a.name].map(&.inspect)
 end
