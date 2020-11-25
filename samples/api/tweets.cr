aca67202df8be615c7c3cf6ce96a0f19ff4d4ce0
maiha <maiha@wota.jp>
2020-11-25 09:04:38 +0900

    proto: add user_name, user_screen_name to Tweet

--- samples/api/tweets.cr
+++ samples/api/tweets.cr
@@ -6,7 +6,7 @@ client.accounts(count: 2).each do |a|
   client.tweets(a.id, count: 3).each do |t|
     puts "+ Tweets(%s, %s) # %s" % [t.account_id.inspect, t.id.inspect, t.id]
     puts "    %s" % [t.created_at]
-    puts "    %s @%s (%s)" % [t.user.try(&.name), t.user.try(&.screen_name), t.user_id]
+    puts "    %s @%s (%s)" % [t.user_name, t.user_screen_name, t.user_id]
     puts "    %s" % [t.full_text.inspect]
   end
 end
