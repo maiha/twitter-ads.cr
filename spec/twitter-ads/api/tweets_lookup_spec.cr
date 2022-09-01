5d97bf9c81cde800650afd89314defdae7f5bd13
norihito-sato-ss <norihito.sato@supership.jp>
2022-09-01 20:10:26 +0900

    [wip]

--- spec/twitter-ads/api/tweets_lookup_spec.cr
+++ spec/twitter-ads/api/tweets_lookup_spec.cr
@@ -6,9 +6,7 @@ describe TwitterAds::Api::TweetsLookup do
   describe "#tweets_lookup" do
     it "returns TwitterAds::TweetsLookup" do
       tweets_lookup = client.tweets_lookup(id: ["1519781379172495360"])
-      pp! tweets_lookup
       tweets_lookup.size.should eq 1
-
       tweets_lookup = tweets_lookup.first
       tweets_lookup.id.should eq "1519781379172495360"
     end
